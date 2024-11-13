41f93f423979518222fa9463be182fcea8da7bdd
mKO9876 <martakokic98@gmail.com>
2024-11-13 20:19:17 +0100

    "New transaction" feature added
    Also edited function UserExists to return int for "New transaction" feature

--- Program.cs
+++ Program.cs
@@ -2,10 +2,15 @@
 using System.Runtime.InteropServices;
 using System.Xml.Linq;
 
-//VRATI SVE NAZAD NA DATETIME, DATEONLY NE SPREMA GODINU IZ NEKOG RAZLOGA
-Dictionary<int, Tuple<string, string, DateTime>> user_list = new Dictionary<int, Tuple<string, string, DateTime>>();
-int next_id = 0;
-//za financije mogu napraviti dict sa kombinacijom stringa i int (string bi bio račun, a int bi bio broj korisnika
+Dictionary<int, Tuple<string, string, DateTime>> user_list = new();
+Dictionary<(int, string), float> account_list = new();
+Dictionary<Tuple<int, int>, Tuple<float, bool, string, string, DateTime>> transaction_list = new();
+//         id_korisnika, "tekući"          id_transakcije|(iznos, tip, kategorija, opis, datum provedbe)
+//bool je za provjeriti je li prihod
+int next_user_id = 0;
+int next_transaction_id = 0;
+string[] acc_names = ["tekući", "žiro", "prepaid"];
+
 Console.WriteLine("DOBRODOŠLI");
 while (true)
 {
@@ -13,16 +18,16 @@ while (true)
     string user_choice = CheckUserInput("Akcija: ");
 
     if (user_choice == "1") Users();
-    else if (user_choice == "2") Console.WriteLine("Odabrana akcija: Račun");
+    else if (user_choice == "2") Accounts();
     else if (user_choice == "3") break;
     else Console.WriteLine("Nepoznat odgovor, pokušajte ponovno.\n");
 }
 
 bool Users()
 {
-        Console.WriteLine("Odabrali ste: Korisnici. ");
-        Console.WriteLine("1 - Unos novog korisnika\n2 - Brisanje korisnika\n3 - Uređivanje korsnika\n4 - Pregled korsnika\n");
-        string user_choice = CheckUserInput("Akcija: ");
+    Console.WriteLine("Odabrali ste: Korisnici. ");
+    Console.WriteLine("1 - Unos novog korisnika\n2 - Brisanje korisnika\n3 - Uređivanje korsnika\n4 - Pregled korsnika\n");
+    string user_choice = CheckUserInput("Akcija: ");
 
     if (user_choice == "1")
     {
@@ -32,13 +37,15 @@ bool Users()
             string last_name = CheckUserInput("Unesite prezime: ");
 
             DateTime birth_date = GetDate();
-            bool user_exists = UserExists(name, last_name);
-            if (!user_exists)
+            if (UserExists(name, last_name) == -1)
             {
                 Tuple<string, string, DateTime> new_user = Tuple.Create(name.ToLower(), last_name.ToLower(), birth_date);
-                user_list[next_id] = new_user;
-                next_id++;
+                user_list[next_user_id] = new_user;
                 //napravi mu 3 računa: prepaid, tekući (ima 100 eura) i žiro, svi ostali imaju 0
+                account_list[(next_user_id, "tekući")] = 100f;
+                account_list[(next_user_id, "žiro")] = 0f;
+                account_list[(next_user_id, "prepaid")] = 0f;
+                next_user_id++;
                 Console.WriteLine("Izrađen novi korisnik.\nVraćanje na početak\n");
                 return true;
             }
@@ -67,7 +74,7 @@ bool Users()
 
                 foreach (KeyValuePair<int, Tuple<string, string, DateTime>> user in user_list)
                 {
-                    if (user.Value.Item1 == name.ToLower() && user.Value.Item2 == last_name.ToLower())
+                    if (string.Equals(user.Value.Item1, name) && string.Equals(user.Value.Item2, last_name))
                     {
                         user_list.Remove(user.Key);
                         Console.WriteLine("Izbrisan korisnik.\n");
@@ -105,16 +112,15 @@ bool Users()
         while (true)
         {
             string user_input = CheckUserInput("Unesite ID: ");
-            int id;
 
-            if (Int32.TryParse(user_input, out id))
+            if (Int32.TryParse(user_input, out int id))
             {
                 if (user_list.ContainsKey(id))
                 {
                     string name = CheckUserInput("Unesite ime: ");
                     string last_name = CheckUserInput("Unesite prezime: ");
                     DateTime birth_date = GetDate();
-                    if (!UserExists(name, last_name))
+                    if (UserExists(name, last_name)>-1)
                     {
                         Tuple<string, string, DateTime> edit_user = Tuple.Create(name, last_name, birth_date);
                         user_list[id] = edit_user;
@@ -150,7 +156,7 @@ bool Users()
                     var dict_sorted = user_list.OrderByDescending(user => user.Value.Item2);
                     foreach (KeyValuePair<int, Tuple<string, string, DateTime>> user in user_list)
                     {
-                        Console.WriteLine($"{user.Key.ToString()} - {user.Value.Item1} - {user.Value.Item2} - {user.Value.Item3.Date.ToString("dd-MM-yyyy")}");
+                        Console.WriteLine($"{user.Key} - {user.Value.Item1} - {user.Value.Item2} - {user.Value.Item3.Date.ToString("dd-MM-yyyy")}");
                     }
                     Console.WriteLine("\n");
                     return true;
@@ -164,7 +170,7 @@ bool Users()
                     foreach (KeyValuePair<int, Tuple<string, string, DateTime>> user in user_list)
                     {
                         int age = today.Year - user.Value.Item3.Year;
-                        if (age > 30) Console.WriteLine($"{user.Key.ToString()} - {user.Value.Item1} - {user.Value.Item2} - {user.Value.Item3.ToString("dd-MM-YYYY")}");
+                        if (age > 30) Console.WriteLine($"{user.Key} - {user.Value.Item1} - {user.Value.Item2} - {user.Value.Item3.ToString("dd-MM-YYYY")}");
                     }
                     Console.WriteLine("\n");
                     return true;
@@ -183,26 +189,67 @@ bool Users()
     }
 }
 
+bool Accounts()
+{
+    Console.WriteLine("Odabrali ste: Računi. ");
+    string name = CheckUserInput("Unesite ime: ");
+    string last_name = CheckUserInput("Unesite prezime: ");
+    while (true)
+    {
+        int user_id = UserExists(name, last_name);
+        if (user_id>-1)
+        {
+            Console.WriteLine("1 - Unos nove transakcije\n2 - Brisanje transakcije\n3 - Uređivanje transakcije\n4 - Pregled transakcija\n6 - Financijsko izvješće\n");
+            string user_choice = CheckUserInput("Akcija: ");
+            if (user_choice == "1")
+            {
+                string user_input_value = CheckUserInput("Unesite iznos: ");
+                string category = CheckUserInput("Unesite kategoriju: ");
+                string description = CheckUserInput("Unesite opis: ");
+
+                string user_account = CheckUserInput("Račun na koji želite prenijeti novac (tekući, žiro, prepaid): ");
+
+                if(float.TryParse(user_input_value, out float value) && acc_names.Contains(user_account))
+                {
+                    Tuple<float, bool, string, string, DateTime> transaction_detail = Tuple.Create(value, true, category, description, DateTime.Now);
+                    Tuple<int, int> transation_key = Tuple.Create(user_id, next_transaction_id);
+                    transaction_list[transation_key] = transaction_detail;
+                    next_transaction_id++;
+                    //uplati na račun
+                    account_list[(user_id, user_account)] += value;
+                    Console.WriteLine("Uspješno obacljena transakcija");
+                }
+                Console.WriteLine("Neuspješna transakcija, pokušajte ponovno.\n");
+            }
+            else if (user_choice == "2") { }
+            else if (user_choice == "3") { }
+            else if (user_choice == "4") { }
+            else if (user_choice == "5") { }
+            else Console.WriteLine("Nepoznata akcija. Pokušajte ponovno.\n");
+        }
+    }
+}
+
 //POMOĆNE FUNKCIJE
 
-bool UserExists(string name, string last_name)
+int UserExists(string name, string last_name)
 {
     foreach (KeyValuePair<int, Tuple<string, string, DateTime>> user in user_list)
     {
-        if (user.Value.Item1.ToLower() == name.ToLower() && user.Value.Item2.ToLower() == last_name.ToLower()) return true;
+        if (string.Equals(user.Value.Item1, name) && string.Equals(user.Value.Item2, last_name)) return user.Key;
     }
-    return false;
+    return -1;
 }
+
 
 DateTime GetDate()
 {
-    DateTime birth_date;
     DateTime today = DateTime.Today;
     while (true)
     {
         Console.Write("Unesite datum rođenja u obliku DD-MM-YYYY: ");
         string? date_string = Console.ReadLine();
-        if (DateTime.TryParse(date_string, out birth_date))
+        if (DateTime.TryParse(date_string, out DateTime birth_date))
         {
             if (birth_date.Year < today.Year && birth_date.Year > (today.Year - 100)) return birth_date;
         }
